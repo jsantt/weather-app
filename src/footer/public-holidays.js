ef531a4301397f3548db3f0499316d56312ea17c
jsantt <35174533+jsantt@users.noreply.github.com>
2020-07-12 10:50:18 +0300

    use iron-icon

--- src/footer/public-holidays.js
+++ src/footer/public-holidays.js
@@ -1,5 +1,6 @@
 import { css, html, LitElement } from 'lit-element';
 
+import '../all-icons.js';
 import './footer-section.js';
 
 class PublicHolidays extends LitElement {
@@ -75,19 +76,7 @@ class PublicHolidays extends LitElement {
         )}
         <div slot="footer-left"></div>
         <div slot="footer-right">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 0 24 24"
-            width="24"
-            class="calendar-logo"
-          >
-            <path d="M0 0h24v24H0V0z" fill="none" />
-            <path d="M5 8h14V6H5z" opacity=".3" />
-            <path
-              d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10zm0-12H5V6h14v2zm-7 5h5v5h-5z"
-            />
-          </svg>
+          <iron-icon icon="all-icons:calendar"></iron-icon>
           <a href="https://www.paivyri.fi/">kalenteri</a>
         </div>
       </footer-section>
