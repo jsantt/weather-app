4a524f9485209f443d92eed3bf6188ba512c895d
Jani Säntti <jani.santti@gmail.com>
2020-07-28 22:24:11 +0300

    remove unused small symbols

--- src/common/weather-symbol-small.js
+++ src/common/weather-symbol-small.js
@@ -1,6 +1,11 @@
 import { css, html, LitElement } from 'lit-element';
 import '@polymer/iron-icon/iron-icon.js';
 
+/**
+ * Snow: 41, 42, 43, 51, 52, 53 / [40-60]
+ * thunder: 61, 62, 63, 64
+ * slush: 71, 72, 73, 81, 82, 83
+ */
 class WeatherSymbolSmall extends LitElement {
   static get is() {
     return 'weather-symbol-small';
@@ -21,9 +26,13 @@ class WeatherSymbolSmall extends LitElement {
   }
 
   render() {
-    return html`<iron-icon
-      .icon="${'weather-symbol-small-icons:weatherSymbol' + this.symbolId}"
-    ></iron-icon> `;
+    if (this._getRightIcon(this.symbolId) === undefined) {
+      return html``;
+    }
+
+    return html` <iron-icon
+      .icon="weather-symbol-small-icons:${this._getRightIcon(this.symbolId)}"
+    ></iron-icon>`;
   }
 
   static get properties() {
@@ -33,6 +42,25 @@ class WeatherSymbolSmall extends LitElement {
         reflect: true,
       },
     };
+  }
+
+  constructor() {
+    super();
+
+    this.icons = [
+      { min: 40, max: 60, symbolName: 'snow' },
+      { min: 60, max: 70, symbolName: 'thunder' },
+      { min: 70, max: 90, symbolName: 'slush' },
+    ];
+  }
+
+  _getRightIcon(symbol) {
+    for (const item of this.icons) {
+      if (item.min < symbol && symbol < item.max) {
+        return item.symbolName;
+      }
+    }
+    return undefined;
   }
 }
 
